28d26456e31a56588e3fa6d11326ac33cb09be0e
coord.e <me@coord-e.com>
2016-12-13 18:15:51 +0900

    Added Character-based posture control.

--- posture/posture.cpp
+++ posture/posture.cpp
@@ -5,55 +5,103 @@
 
 using namespace yamax;
 
-posture::posture(const std::string& rname) : joints(getBusnumFromJson(getJsonFromFile(rname)),getServonumFromJson(getJsonFromFile(rname)),getAddressFromJson(getJsonFromFile(rname)))
+posture::posture(const std::string& rname) : servonum(getServonumFromJson(getJsonFromFile(rname))), joints(getBusnumFromJson(getJsonFromFile(rname)),getServonumFromJson(getJsonFromFile(rname)),getAddressFromJson(getJsonFromFile(rname)))
 {
   _path = "/usr/share/actions/"+rname+"/";
 
   joints.setPWMFreq(50);
 
-  std::string standpath = getJsonFromFile(rname).get<picojson::object>()["stand"].get<std::string>();
+  picojson::value v  = getJsonFromFile(rname);
+  const picojson::value::object& obj = v.get<picojson::object>()["map"].get<picojson::object>();
+  for (picojson::value::object::const_iterator i = obj.begin();
+       i != obj.end();
+       ++i) {
+    //std::cout << "mapping. servo: " << i->first.c_str()[0] << " num: " << i->second.get<double>() << std::endl;
+    mapping[i->first.c_str()[0]] = (uint8_t)i->second.get<double>();
+  }
+
+  std::string standpath = v.get<picojson::object>()["stand"].get<std::string>();
   std::ifstream ifs(_path+standpath);
   std::string str;
   if (ifs.fail())
       throw std::runtime_error("Failed to open configuration file: " + _path+standpath);
 
-  int j=0;
-  while (getline(ifs, str))
-  {
-    std::string::size_type index = str.find("#");
-    if (index != std::string::npos)
-      str = str.substr(0, index);
-    _stands.push_back(atoi(str.c_str()));
-    j++;
+  picojson::value standv;
+  ifs >> standv;
+  std::string err = picojson::get_last_error();
+  if (err.empty() && standv.is<picojson::object>()){
+    const picojson::value::object& obj = standv.get<picojson::object>();
+    _stands.resize(servonum);
+    for (picojson::value::object::const_iterator i = obj.begin();
+         i != obj.end();
+         ++i) {
+      //std::cout << "Setting stand. servo: " << i->first.c_str()[0] << " num: " << (int)mapping[i->first.c_str()[0]] << " deg: " << i->second.get<double>() << std::endl;
+      _stands[mapping[i->first.c_str()[0]]] = (uint8_t)i->second.get<double>();
+    }
+  }else{
+    int j=0;
+    ifs.seekg(0, std::ios_base::beg);
+    while (getline(ifs, str))
+    {
+      std::string::size_type index = str.find("#");
+      if (index != std::string::npos)
+        str = str.substr(0, index);
+      //std::cout << "Setting stand. num: " << j << " deg: " << str.c_str() << std::endl;
+      _stands.push_back(atoi(str.c_str()));
+      j++;
+    }
+    if(j > joints.getNumber())
+      throw std::runtime_error("Servo number mismatch!");
   }
-  if(j > joints.getNumber())
-    throw std::runtime_error("Servo number mismatch!");
+  ifs.close();
 }
 
 bool posture::setPosture(std::string post)
 {
   if(!fileExists(_path+post)){
-    post += ".f";
-    if(!fileExists(_path+post))
-      return false;
+    if(!fileExists(_path+post+".f")){
+      if(!fileExists(_path+post+".jf")){
+        throw std::runtime_error("posture file not found");
+      }else
+        post+=".jf";
+    }else
+      post+=".f";
   }
+
   std::ifstream ifs(_path+post);
   std::string str;
-
   if (ifs.fail()){
-      return false;
+      throw std::runtime_error("Couldn't open the posture file: "+_path+post);
   }
-  int j=0;
-  while (getline(ifs, str))
-  {
-    std::string::size_type index = str.find("#");
-    if (index != std::string::npos)
-      str = str.substr(0, index);
-    joints[j].setDegrees(atoi(str.c_str())+_stands[j]);
-    j++;
-    if(j >= joints.getNumber())
-     break;
+
+  picojson::value posv;
+  ifs >> posv;
+  std::string err = picojson::get_last_error();
+  if (err.empty() && posv.is<picojson::object>()){
+    const picojson::value::object& obj = posv.get<picojson::object>();
+    _stands.resize(servonum);
+    for (picojson::value::object::const_iterator i = obj.begin();
+         i != obj.end();
+         ++i) {
+      //std::cout << "Setting posture. servo: " << i->first.c_str()[0] << " num: " << (int)mapping[i->first.c_str()[0]] << " deg: " << i->second.get<double>() << std::endl;
+      joints[mapping[i->first.c_str()[0]]].setDegrees((int8_t)i->second.get<double>()+_stands[mapping[i->first.c_str()[0]]]);
+    }
+  }else{
+    int j=0;
+    ifs.seekg(0, std::ios_base::beg);
+    while (getline(ifs, str))
+    {
+      std::string::size_type index = str.find("#");
+      if (index != std::string::npos)
+        str = str.substr(0, index);
+      //std::cout << "Setting posture. num: " << j << " deg: " << str.c_str() << std::endl;
+      joints[j].setDegrees(atoi(str.c_str())+_stands[j]);
+      j++;
+    }
+    if(j > joints.getNumber())
+      throw std::runtime_error("Servo number mismatch!");
   }
+  ifs.close();
   return true;
 }
 
@@ -71,7 +119,7 @@ bool posture::fileExists(const std::string& path)
 
 picojson::value posture::getJsonFromFile(const std::string& name)
 {
-  if (! vnow.is<picojson::object>()) {
+  if (! _vnow.is<picojson::object>()) {
     std::ifstream jsonifs("/usr/share/actions/"+name+"/robot.conf");
     if (jsonifs.fail())
       throw std::runtime_error("Failed to open configuration file: " + name);
@@ -85,10 +133,11 @@ picojson::value posture::getJsonFromFile(const std::string& name)
     if (! v.is<picojson::object>())
       throw std::runtime_error("json isn't an object");
 
-    vnow = v;
+    _vnow = v;
+    jsonifs.close();
     return v;
   }else
-    return vnow;
+    return _vnow;
 }
 
 uint16_t posture::getServonumFromJson(picojson::value v)
--- posture/posture.hpp
+++ posture/posture.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <map>
 #include "picojson.h"
 #include "physicalio/physicalio.hpp"
 
@@ -13,14 +14,16 @@ namespace yamax
       posture(const std::string& rname);
       bool setPosture(const std::string post);
       void stand();
+      uint16_t servonum;
       servos joints;
+      std::map<char, uint8_t> mapping;
 
   private:
       picojson::value getJsonFromFile(const std::string& name);
       uint16_t getServonumFromJson(picojson::value v);
       uint8_t getBusnumFromJson(picojson::value v);
       uint8_t getAddressFromJson(picojson::value v);
-      picojson::value vnow;
+      picojson::value _vnow;
       bool fileExists(const std::string& path);
       std::string _path;
       std::vector<uint8_t> _stands;
